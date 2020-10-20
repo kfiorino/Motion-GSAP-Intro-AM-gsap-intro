82ffca4862b463f1f483a968bde7a4362e693a87
kfiorino <63323923+kfiorino@users.noreply.github.com>
2020-10-20 10:32:34 -0400

    recent

--- dev/js/demo.js
+++ dev/js/demo.js
@@ -2,9 +2,10 @@ import {gsap} from "gsap";
 
 const toggleTL = gsap.timeline();
 
-toggleTL.to("#background", {duration: 0.5, fill:"#95D6FA"})
-    .to("#dog-button", {duration: 0.5, fill:"#FFE5A1"})
-    .to("#cat-button", {duration: 0.5, fill:"#F9BB28"});
+toggleTL.to("#background", {duration: 0.05, fill:"#95D6FA"})
+    .to("#cat-button", {duration: 0.05, fill:"#D8D8D8"})
+    .to("#dog-button", {duration: 0.05, fill:"#3575B9"});
+    
 
 export function toggleAnimation(){
     return toggleTL;
